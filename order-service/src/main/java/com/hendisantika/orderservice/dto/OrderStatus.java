031389c31da8dd1afb923f6f6251878668743954
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 04:38:30 +0700

    Add OrderStatus in order-service 🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/com/hendisantika/orderservice/dto/OrderStatus.java
@@ -0,0 +1,19 @@
+package com.hendisantika.orderservice.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 04:38
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrderStatus {
+
+    ORDER_CREATED,
+    ORDER_CANCELLED,
+    ORDER_COMPLETED
+
+}
