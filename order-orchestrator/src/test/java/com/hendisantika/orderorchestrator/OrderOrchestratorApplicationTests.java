16d17cd3940a977edf9d296abe0567073657dfd1
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:02:36 +0700

    Add order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/test/java/com/hendisantika/orderorchestrator/OrderOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.orderorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
