64cb3fd26a2217791befe31faa44febf53521270
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:07:41 +0700

    Add OrderStatus in order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/common/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.hendisantika.orderorchestrator.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 05:07
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrderStatus {
+    ORDER_CREATED,
+    ORDER_CANCELLED,
+    ORDER_COMPLETED
+
+}
