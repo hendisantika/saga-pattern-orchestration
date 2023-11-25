52d3a00755424177a4dcfbc0390a0b86127dc48f
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 04:53:15 +0700

    Add getOrders in OrderController in order-service 🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/com/hendisantika/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/hendisantika/orderservice/controller/OrderController.java
@@ -1,13 +1,12 @@
 package com.hendisantika.orderservice.controller;
 
 import com.hendisantika.orderservice.dto.OrderRequestDTO;
+import com.hendisantika.orderservice.dto.OrderResponseDTO;
 import com.hendisantika.orderservice.entity.PurchaseOrder;
 import com.hendisantika.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -30,5 +29,10 @@ public class OrderController {
     @PostMapping("/create")
     public Mono<PurchaseOrder> createOrder(@RequestBody Mono<OrderRequestDTO> orderMono) {
         return orderMono.flatMap(orderService::createOrder);
+    }
+
+    @GetMapping("/all")
+    public Flux<OrderResponseDTO> getOrders() {
+        return orderService.getAllOrder();
     }
 }
