b8bdfea3575c819c45b9d3187c3b75db512269dc
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:09:26 +0700

    Add PaymentStatus in order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/common/PaymentStatus.java
@@ -0,0 +1,16 @@
+package com.hendisantika.orderorchestrator.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 05:09
+ * To change this template use File | Settings | File Templates.
+ */
+public enum PaymentStatus {
+    PAYMENT_APPROVED,
+    PAYMENT_REJECTED
+}
