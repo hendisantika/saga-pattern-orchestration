5e8553ca14e7b0c709c086443cbbe7d491886524
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 04:58:44 +0700

    Add PaymentResponseDTO in payment-service 🫸🌀✏️📗🐧🐳

---
+++ payment-service/src/main/java/com/hendisantika/paymentservice/dto/PaymentResponseDTO.java
@@ -0,0 +1,30 @@
+package com.hendisantika.paymentservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 04:58
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@ToString
+public class PaymentResponseDTO {
+    private Integer userId;
+
+    private UUID orderId;
+
+    private Double amount;
+
+    private PaymentStatus status;
+}
