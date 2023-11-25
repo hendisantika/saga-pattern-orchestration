e33da2ea5c5f98832adc3f4045e6c3774f2b5492
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 04:59:47 +0700

    Add debit in PaymentService in payment-service 🫸🌀✏️📗🐧🐳

--- payment-service/src/main/java/com/hendisantika/paymentservice/service/PaymentService.java
+++ payment-service/src/main/java/com/hendisantika/paymentservice/service/PaymentService.java
@@ -1,5 +1,8 @@
 package com.hendisantika.paymentservice.service;
 
+import com.hendisantika.paymentservice.dto.PaymentRequestDTO;
+import com.hendisantika.paymentservice.dto.PaymentResponseDTO;
+import com.hendisantika.paymentservice.dto.PaymentStatus;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -30,5 +33,24 @@ public class PaymentService {
         paymentMap.put(1, 500d);
         paymentMap.put(2, 1000d);
         paymentMap.put(3, 700d);
+    }
+
+    public PaymentResponseDTO debit(PaymentRequestDTO requestDTO) {
+        double balance = paymentMap.getOrDefault(requestDTO.getUserId(), 0d);
+
+        PaymentResponseDTO responseDTO = new PaymentResponseDTO();
+        responseDTO.setOrderId(requestDTO.getOrderId());
+        responseDTO.setUserId(requestDTO.getUserId());
+        responseDTO.setAmount(requestDTO.getAmount());
+        responseDTO.setStatus(PaymentStatus.PAYMENT_REJECTED);
+
+        log.info("Inside Payment:: " + balance);
+
+        if (balance >= requestDTO.getAmount()) {
+            responseDTO.setStatus(PaymentStatus.PAYMENT_APPROVED);
+            paymentMap.put(requestDTO.getUserId(), balance - requestDTO.getAmount());
+        }
+
+        return responseDTO;
     }
 }
