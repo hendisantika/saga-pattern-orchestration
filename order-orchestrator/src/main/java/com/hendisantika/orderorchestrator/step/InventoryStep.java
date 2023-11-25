1f566ff121ea7c9b9e672a30e84fb246ff964472
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:17:17 +0700

    Add revert in InventoryStep in order-orchestrator 🫸🌀✏️📗🐧🐳

--- order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/step/InventoryStep.java
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/step/InventoryStep.java
@@ -46,4 +46,16 @@ public class InventoryStep implements WorkflowStep {
                 .map(r -> r.getStatus().equals(InventoryStatus.AVAILABLE))
                 .doOnNext(b -> stepStatus = b ? WorkflowStepStatus.COMPLETE : WorkflowStepStatus.FAILED);
     }
+
+    @Override
+    public Mono<Boolean> revert() {
+        return webClient
+                .post()
+                .uri("/inventory/add")
+                .body(BodyInserters.fromValue(requestDTO))
+                .retrieve()
+                .bodyToMono(Void.class)
+                .map(r -> true)
+                .onErrorReturn(false);
+    }
 }
