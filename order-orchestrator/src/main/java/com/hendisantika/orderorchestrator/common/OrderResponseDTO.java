94da1ed2bbcaa9163f8470cf2ba11b238f32fe2f
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:08:39 +0700

    Add OrderResponseDTO in order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/common/OrderResponseDTO.java
@@ -0,0 +1,28 @@
+package com.hendisantika.orderorchestrator.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 05:08
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@ToString
+public class OrderResponseDTO {
+    private UUID orderId;
+    private Integer userId;
+    private Integer productId;
+    private Double amount;
+    private OrderStatus status;
+}
