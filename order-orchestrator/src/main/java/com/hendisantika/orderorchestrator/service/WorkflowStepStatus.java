4d5b2f3dcd2f3465e91e3febe64be0319d21b69d
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:15:03 +0700

    Add WorkflowStepStatus in order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/service/WorkflowStepStatus.java
@@ -0,0 +1,17 @@
+package com.hendisantika.orderorchestrator.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 05:14
+ * To change this template use File | Settings | File Templates.
+ */
+public enum WorkflowStepStatus {
+    PENDING,
+    COMPLETE,
+    FAILED
+}
