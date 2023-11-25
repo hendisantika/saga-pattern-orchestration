44c844495db1e4043d4c8d531b8d893bc958d15e
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:05:43 +0700

    Add InventoryStatus in order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/common/InventoryStatus.java
@@ -0,0 +1,16 @@
+package com.hendisantika.orderorchestrator.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 05:05
+ * To change this template use File | Settings | File Templates.
+ */
+public enum InventoryStatus {
+    PAYMENT_APPROVED,
+    PAYMENT_REJECTED
+}
