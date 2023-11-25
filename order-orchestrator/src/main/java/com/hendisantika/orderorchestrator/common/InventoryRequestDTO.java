a44d105af80dfa9f1accb5624b5569820f5d68eb
hendisantika <hendisantika@yahoo.co.id>
2023-11-26 05:04:03 +0700

    Add InventoryRequestDTO in order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/common/InventoryRequestDTO.java
@@ -0,0 +1,26 @@
+package com.hendisantika.orderorchestrator.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/26/23
+ * Time: 05:03
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@ToString
+public class InventoryRequestDTO {
+    private Integer userId;
+    private Integer productId;
+    private UUID orderId;
+}
