8eb8c19c7e37940d9f00bd8343194727b2343b01
hendisantika <hendisantika@yahoo.co.id>
2023-12-02 06:02:48 +0700

    Add WebClientConfig in order-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ order-orchestrator/src/main/java/com/hendisantika/orderorchestrator/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.hendisantika.orderorchestrator.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/2/23
+ * Time: 06:02
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebClientConfig {
+}
