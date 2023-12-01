5b5fdb8d12d23863a7e0caf86dc9641779f71df1
hendisantika <hendisantika@yahoo.co.id>
2023-12-02 06:13:18 +0700

    Add deduct in InventoryService in investor-orchestrator 🫸🌀✏️📗🐧🐳

--- inventory-service/src/main/java/com/hendisantika/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/hendisantika/inventoryservice/service/InventoryService.java
@@ -1,5 +1,8 @@
 package com.hendisantika.inventoryservice.service;
 
+import com.hendisantika.inventoryservice.dto.InventoryRequestDTO;
+import com.hendisantika.inventoryservice.dto.InventoryResponseDTO;
+import com.hendisantika.inventoryservice.dto.InventoryStatus;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +31,24 @@ public class InventoryService {
         inventoryMap.put(1, 2);
         inventoryMap.put(2, 3);
         inventoryMap.put(3, 4);
+    }
+
+    public InventoryResponseDTO deduct(InventoryRequestDTO requestDTO) {
+        int qty = inventoryMap.getOrDefault(requestDTO.getProductId(), 0);
+
+        InventoryResponseDTO responseDTO = new InventoryResponseDTO();
+        responseDTO.setOrderId(requestDTO.getOrderId());
+        responseDTO.setProductId(requestDTO.getProductId());
+        responseDTO.setUserId(requestDTO.getUserId());
+        responseDTO.setStatus(InventoryStatus.UNAVAILABLE);
+
+        System.out.println("Inside Inventory::" + qty);
+
+        if (qty > 0) {
+            responseDTO.setStatus(InventoryStatus.AVAILABLE);
+            inventoryMap.put(requestDTO.getProductId(), qty - 1);
+        }
+
+        return responseDTO;
     }
 }
