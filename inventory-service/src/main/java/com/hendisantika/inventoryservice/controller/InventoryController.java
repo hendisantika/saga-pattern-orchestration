118da88647976bd52324f6475262311de6818316
hendisantika <hendisantika@yahoo.co.id>
2023-12-02 06:14:45 +0700

    Add deduct in InventoryController in investor-orchestrator 🫸🌀✏️📗🐧🐳

--- inventory-service/src/main/java/com/hendisantika/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/hendisantika/inventoryservice/controller/InventoryController.java
@@ -1,7 +1,11 @@
 package com.hendisantika.inventoryservice.controller;
 
+import com.hendisantika.inventoryservice.dto.InventoryRequestDTO;
+import com.hendisantika.inventoryservice.dto.InventoryResponseDTO;
 import com.hendisantika.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class InventoryController {
 
     private final InventoryService inventoryService;
+
+    @PostMapping("/deduct")
+    public InventoryResponseDTO deduct(@RequestBody InventoryRequestDTO rqRequestDTO) {
+        return inventoryService.deduct(rqRequestDTO);
+
+    }
 }
