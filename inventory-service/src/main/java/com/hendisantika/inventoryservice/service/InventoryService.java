2c56560b2caebed37e7dc1ce3bac53c23d6416a7
hendisantika <hendisantika@yahoo.co.id>
2023-12-02 06:12:48 +0700

    Add InventoryService in investor-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ inventory-service/src/main/java/com/hendisantika/inventoryservice/service/InventoryService.java
@@ -0,0 +1,21 @@
+package com.hendisantika.inventoryservice.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/2/23
+ * Time: 06:12
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class InventoryService {
+
+    private Map<Integer, Integer> inventoryMap;
+}
