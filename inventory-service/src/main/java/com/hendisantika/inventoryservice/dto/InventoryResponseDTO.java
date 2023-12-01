decd6c5df8fbeec8103e6c684ca83977e9ff1adf
hendisantika <hendisantika@yahoo.co.id>
2023-12-02 06:12:09 +0700

    Add InventoryResponseDTO in investor-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ inventory-service/src/main/java/com/hendisantika/inventoryservice/dto/InventoryResponseDTO.java
@@ -0,0 +1,30 @@
+package com.hendisantika.inventoryservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/2/23
+ * Time: 06:11
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@ToString
+public class InventoryResponseDTO {
+    private Integer userId;
+
+    private Integer productId;
+
+    private UUID orderId;
+
+    private InventoryStatus status;
+}
