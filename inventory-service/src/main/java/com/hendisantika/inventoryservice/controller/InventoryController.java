1b2dddea60ec183b7d04ccdd9e9857cca33d1ea7
hendisantika <hendisantika@yahoo.co.id>
2023-12-02 06:14:25 +0700

    Add InventoryController in investor-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ inventory-service/src/main/java/com/hendisantika/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.inventoryservice.controller;
+
+import com.hendisantika.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/2/23
+ * Time: 06:13
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+}
