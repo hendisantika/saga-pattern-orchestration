4a5efa49f658a2456ce042f047abdbcf5474f01a
hendisantika <hendisantika@yahoo.co.id>
2023-12-02 06:11:48 +0700

    Add InventoryStatus in investor-orchestrator 🫸🌀✏️📗🐧🐳

---
+++ inventory-service/src/main/java/com/hendisantika/inventoryservice/dto/InventoryStatus.java
@@ -0,0 +1,16 @@
+package com.hendisantika.inventoryservice.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : saga-pattern-orchestration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/2/23
+ * Time: 06:11
+ * To change this template use File | Settings | File Templates.
+ */
+public enum InventoryStatus {
+    AVAILABLE,
+    UNAVAILABLE
+}
